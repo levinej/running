312616e443a6afe02a67b0e2e7efc626dbe2d212
levinej <josh@gsas-air-2.home>
2014-07-20 13:39:56 -0600

    Adding help files

---
+++ help-nike.php
@@ -0,0 +1,25 @@
+<?php
+#phpinfo();
+
+  require_once 'nikeplusphp.4.5.1.php';
+  require 'cindyruns.php';
+  $n = new NikePlusPHP('$cindyrunsUsername', '$cindyrunsPassword');
+
+  $activities = $n->activities();
+echo "Activities:<pre>";
+var_dump($activities);  
+echo "</pre><br>";
+
+$alltime = $n->alltime();
+echo "All Time:<pre>";
+var_dump($alltime);
+echo "</pre><br>";
+
+$mostrecent = $n->mostRecentActivity();
+echo "Most Recent:<pre>";
+var_dump($mostrecent);
+echo "</pre><br>";
+
+  $totalMiles = $n->toMiles($alltime->lifetimeTotals->distance);
+echo $totalMiles;
+?>
